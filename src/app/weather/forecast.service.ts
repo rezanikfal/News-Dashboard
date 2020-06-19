c37353d4cc0dba5330c879ca12ce8c90f4416e90
Mohammadreza Nikfal <mohammadreza.nikfal@bencorinc.com>
2020-06-19 09:57:14 -0500

    Get weather forecast Log

--- src/app/weather/forecast.service.ts
+++ src/app/weather/forecast.service.ts
@@ -1,21 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpParams, HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs';
+import { map, switchMap } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ForecastService {
+private url = 'https://api.openweathermap.org/data/2.5/forecast'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getCurrentLocation(){
-    return new Observable(observer =>{
+  getForecast() {
+    return this.getCurrentLocation()
+      .pipe(
+        map(coords => {
+          return new HttpParams()
+            .set('lat', String(coords.latitude))
+            .set('lon', String(coords.longitude))
+            .set('units', 'metric')
+            .set('appid', '8b2b98ac09805bc077428235701c84be')
+        }),
+        switchMap(params => this.http.get(this.url, {params})
+        )
+      )
+  }
+
+  getCurrentLocation() {
+    return new Observable<Coordinates>(observer => {
       window.navigator.geolocation.getCurrentPosition(
-        position =>{
+        position => {
           observer.next(position.coords)
           observer.complete()
         },
-        err=>observer.error(err)
+        err => observer.error(err)
       )
     })
   }
--- src/app/weather/forecast/forecast.component.ts
+++ src/app/weather/forecast/forecast.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ForecastService } from "../forecast.service";
 
 @Component({
   selector: 'app-forecast',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForecastComponent implements OnInit {
 
-  constructor() { }
+  constructor( forecastService:ForecastService) { 
+    forecastService.getForecast().subscribe(weatherResponse =>{
+      console.log(weatherResponse);
+    })
+  }
 
   ngOnInit(): void {
   }
